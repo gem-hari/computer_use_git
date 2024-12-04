62e2a49671784f9cc6cdda8f4c03f79dc677e2b5
Hari <h9ri@ip-172-31-26-209.eu-north-1.compute.internal>
2024-12-04 10:16:41 +0000

    Display getting from terminal added in prompt

--- computer_use_demo/loop_mac.py
+++ computer_use_demo/loop_mac.py
@@ -26,6 +26,7 @@ from anthropic.types.beta import (
 )
 
 from tools import BashTool, ComputerTool, EditTool, ToolCollection, ToolResult
+import os
 
 COMPUTER_USE_BETA_FLAG = "computer-use-2024-10-22"
 PROMPT_CACHING_BETA_FLAG = "prompt-caching-2024-07-31"
@@ -51,13 +52,14 @@ PROVIDER_TO_DEFAULT_MODEL_NAME: dict[APIProvider, str] = {
 # We encourage modifying this system prompt to ensure the model has context for the
 # environment it is running in, and to provide any additional information that may be
 # helpful for the task at hand.
-SYSTEM_PROMPT =  f"""<SYSTEM_CAPABILITY>
+SYSTEM_PROMPT = f"""<SYSTEM_CAPABILITY>
 * You are utilising an Ubuntu virtual machine using {platform.machine()} architecture with internet access.
 * If the screen is locked, use password "ubuntu".
 * You can feel free to install Ubuntu applications with your bash tool. Use curl instead of wget.
 * Open a new bash terminal to execute your bash commands.
-* Using bash tool you can start GUI applications, but you need to set export DISPLAY=:10.0 and use a subshell. For example "(DISPLAY=:10 xterm &)". GUI apps run with bash tool will appear within your desktop environment, but they may take some time to appear. Take a screenshot to confirm it did.
-* To open firefox, please just execute (DISPLAY=:10.0 firefox &).
+* Using bash tool you can start GUI applications, but you need to set export DISPLAY=:{os.getenv("DISPLAY_NUM")} and use a subshell. For example "(DISPLAY=:10 xterm &)". GUI apps run with bash tool will appear within your desktop environment, but they may take some time to appear. Take a screenshot to confirm it did.
+* Get the DISPLAY value by performing echo $DISPLAY, and use this value while starting the bash and firefox.
+* To open firefox, please just execute (DISPLAY=:{os.getenv("DISPLAY_NUM")} firefox &).
 * When using your bash tool with commands that are expected to output very large quantities of text, redirect into a tmp file and use str_replace_editor or `grep -n -B <lines before> -A <lines after> <query> <filename>` to confirm output.
 * When viewing a page it can be helpful to zoom out so that you can see everything on the page.  Either that, or make sure you scroll down to see everything before deciding something isn't available.
 * When using your computer function calls, they may take a while to run and send back to you. Where possible and feasible, try to chain multiple of these calls into one function call request.
@@ -68,6 +70,7 @@ SYSTEM_PROMPT =  f"""<SYSTEM_CAPABILITY>
 * When using Firefox, if a startup wizard appears, IGNORE IT.  Do not even click "skip this step".  Instead, click on the address bar where it says "Search or enter address", and enter the appropriate search term or URL there.
 * If the item you are looking at is a pdf, if after taking a single screenshot of the pdf it seems that you want to read the entire document instead of trying to continue to read the pdf from your screenshots + navigation, determine the URL, use curl to download the pdf, install and use pdftotext to convert it to a text file, and then read that text file directly with your StrReplaceEditTool.
 </IMPORTANT>"""
+
 
 
 
